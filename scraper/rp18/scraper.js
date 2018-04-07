fb68389414aefa985ac1ce64274066a7c52ba0b4
Thomas Kollbach <thomas@kollba.ch>
2018-04-07 15:48:55 +0200

    Colorize tracks

--- scraper/rp18/scraper.js
+++ scraper/rp18/scraper.js
@@ -1,8 +1,12 @@
+const path = require('path');
+const fs = require('fs');
 const RpNewImporter = require('./../lib/rp_new/rp_new_importer');
 const events = require('./../config/events');
 const JSONRequester = require('../lib/json_requester');
+const trackColors = require('./track_colors');
 
 const EVENT_ID = 'rp18';
+const dumpFolder = `${path.resolve(__dirname, '../../web/data/')}/${EVENT_ID}/`;
 
 const event = events.find(eventJson => eventJson.id === EVENT_ID);
 if (!event) throw new Error(`Could not find event ${EVENT_ID}`);
@@ -29,9 +33,16 @@ exports.scrape = (callback) => {
             '2018-05-03': 'Tag 2',
             '2018-05-04': 'Tag 3',
           },
+          trackColorMap: trackColors,
         },
       );
 
+      // dump source JSONs
+      fs.mkdir(dumpFolder, () => {
+        fs.writeFileSync(`${dumpFolder}/sessions-source.json`, JSON.stringify(result.sessions, null, '\t'), 'utf8');
+        fs.writeFileSync(`${dumpFolder}/speakers-source.json`, JSON.stringify(result.speakers, null, '\t'), 'utf8');
+      });
+      
       callback(importer.JSON);
     },
   );
